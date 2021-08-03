003cad899c895a8d64a1a0a94a0bbd0eea9493ba
aguenzler <antonio.guenzler@unifr.ch>
2021-08-03 11:40:48 +0200

    generating loop

--- Code/MAIN.jl
+++ Code/MAIN.jl
@@ -11,11 +11,11 @@ include("methods.jl")
 include("depr_vectorized.jl")
 
 #Parameters set by the user (lengths in nm, angles in degrees)
-freq = 400
+freq = 700
 λ = 2.99792458e5/freq      #wavelength in nm
 φ = 90      #azimuthal angle of incidence, do not change in 1D for fixed y-z plane of incidence
 θ = 0       #polar angle of incidence
-NG = 10    #reciprocal lattice cut-off (see Lattice struct in parameters.jl)
+NG = 100    #reciprocal lattice cut-off (see Lattice struct in parameters.jl)
 ϵ_bg = 1 + 0im  #permittivity of background medium
 mat_file = "Ag_JC_nk.txt"   #file storing permittivities of medium in sphere. Format as in refractiveindex.info files
 a = 30.0    #lattice constant
@@ -27,7 +27,7 @@ if mmdim == 1
     V_2 = 2*Rad                              #Volume definition required
 elseif mmdim == 2
     V_2 = pi*Rad^2
-    polydegs = (0,0)    #maximum degrees (N,M) of polynomial to approximate the current in the d-sphere c=
+    polydegs = (2,2)    #maximum degrees (N,M) of polynomial to approximate the current in the d-sphere c=
 end
 
 #Code starts here
@@ -35,5 +35,24 @@ end
 Init_Workspace(λ = λ, φ = φ, θ = θ, NG = NG, ϵ_1 = ϵ_bg,
     ϵ_2 = mat_file, A = A, Rad = Rad, mmdim = mmdim)
 
-kmodes, cvecs = getpolyxMode(polydegs,manual_ks=[0.00016749077375082647 + 0.02663542855877462im,
-0.013559231069713929 + 6.978601628548177e-6im])
+freq_sweep = 700 : 2 : 900
+speed_of_light = 2.99792458e5
+bands_path = string(pwd(), "\\Results\\BS_new.dat")
+open(bands_path, "a") do io
+    write(io, string(now(),"\nFrequency Re(k1) Im(k1) Re(k2) Im(k2)\n"))
+end
+sorttol = 1e-2
+init_k = []
+t1=time()
+for freq in collect(freq_sweep)
+    λ = speed_of_light / freq
+    Init_Workspace(λ = λ, φ = φ, θ = θ, NG = NG, ϵ_1 = ϵ_bg,
+        ϵ_2 = mat_file, A = A, Rad = Rad, mmdim = mmdim)
+    println(p.lambda)
+    kmode = getpolyxMode(polydegs,manual_ks=[init_k])[1]
+    open(bands_path, "a") do io
+        writedlm(io, hcat(real.(freq), real.(kmode[1]), imag.(kmode[1]), real.(kmode[2]), imag(kmode[2])))
+    end
+    init_k = kmode
+    @printf("Runtime for %f nm was %f minutes, finished @ %s\n",wl,(time()-t1)/60,Dates.format(now(), "HHhMM"));global t1=time()
+end
