d8a2fb3d0f0ba3a0f7c48240a858aab45296c054
aguenzler <antonio.guenzler@unifr.ch>
2021-08-04 16:40:41 +0200

    Frequency sweep uses linear interpolation as next guess

--- Code/MAIN.jl
+++ Code/MAIN.jl
@@ -11,8 +11,8 @@ include("methods.jl")
 include("depr_vectorized.jl")
 
 #Parameters set by the user (lengths in nm, angles in degrees)
-freq = 700
-λ = 2.99792458e5/freq      #wavelength in nm
+# freq = 700
+# λ = 2.99792458e5/freq      #wavelength in nm
 φ = 90      #azimuthal angle of incidence, do not change in 1D for fixed y-z plane of incidence
 θ = 0       #polar angle of incidence
 NG = 100    #reciprocal lattice cut-off (see Lattice struct in parameters.jl)
@@ -32,30 +32,38 @@ end
 
 #Code starts here
 
-Init_Workspace(λ = λ, φ = φ, θ = θ, NG = NG, ϵ_1 = ϵ_bg,
-    ϵ_2 = mat_file, A = A, Rad = Rad, mmdim = mmdim)
+# Init_Workspace(λ = λ, φ = φ, θ = θ, NG = NG, ϵ_1 = ϵ_bg,
+#     ϵ_2 = mat_file, A = A, Rad = Rad, mmdim = mmdim)
 
 # init_k = [ 0.0004376494248136995 + 0.019498110004211807im,   0.02883857058875105 + 0.00031740992926030585im] # for 700 THz
 # kmode = getpolyxMode(polydegs,manual_ks=init_k)[1]
 
-freq_sweep = 900 : -2 : 824
+freq_step_size = .5
+freq_sweep = 822 : freq_step_size : 900
 speed_of_light = 2.99792458e5
-bands_path = string(pwd(), "\\Results\\BS_wKappa.dat")
+bands_path = string(pwd(), "\\Results\\BS_wKappa_interpol-test.dat")
 open(bands_path, "a") do io
     write(io, string(now(),"\nFrequency Re(k1) Im(k1) Re(k2) Im(k2)\n"))
 end
 sorttol = 1e-2
-init_k = [0.0012812934048370053 + 0.011393471563159698im, 0.06442695395385803 + 0.017188413925628337im]
+manual_init_k = [0.0012812934048370053 + 0.011393471563159698im, 0.06442695395385803 + 0.017188413925628337im]
+kmodes = zeros(ComplexF64,(size(freq_sweep,1),2))
 t1=time()
-for freq in collect(freq_sweep)
+for (fr, freq) in enumerate(collect(freq_sweep))
     λ = speed_of_light / freq
     Init_Workspace(λ = λ, φ = φ, θ = θ, NG = NG, ϵ_1 = ϵ_bg,
         ϵ_2 = mat_file, A = A, Rad = Rad, mmdim = mmdim)
     println(p.lambda)
-    kmode = getpolyxMode(polydegs,manual_ks=init_k)[1]
-    open(bands_path, "a") do io
-        writedlm(io, hcat(real.(freq), real.(kmode[1]), imag.(kmode[1]), real.(kmode[2]), imag(kmode[2])))
+    if fr == 1
+        init_k = manual_init_k
+    elseif fr == 2
+        init_k = kmodes[fr-1,:]
+    else
+        init_k = 2*kmodes[fr-1,:] - kmodes[fr-2,:]
     end
-    global init_k = kmode
+    kmodes[fr,:] = getpolyxMode(polydegs,manual_ks=init_k)[1]
+    open(bands_path, "a") do io
+        writedlm(io, hcat(real.(freq), real.(kmodes[fr,1]), imag.(kmodes[fr,1]), real.(kmodes[fr,2]), imag(kmodes[fr,2])))
+    end
     @printf("Runtime for %f nm was %f minutes, finished @ %s\n",λ,(time()-t1)/60,Dates.format(now(), "HHhMM"));global t1=time()
 end
